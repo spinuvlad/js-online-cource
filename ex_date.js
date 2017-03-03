c34d3f9bac434583b4bbfe2e2b231d86fd6d744b
Spinu Vladimir <spinu.vlad@gmail.com>
2017-03-03 11:17:06 +0200

    sdds

--- ex_date.js
+++ ex_date.js
@@ -46,3 +46,74 @@ for (var i = 0; i < arrDate.length; i++)
 }
 document.write(arrDate[0] + ':' + arrDate[1] + ':' + arrDate[2] + ' ' + arrDate[3] + '.' + arrDate[4] + '.' + arrDate[5]);  
 //13:37:59 02.02.2017
+-------------------------------- .getDay() --------------------------------
+5. Выведите на экран номер текущего дня недели.
+var date = new Date();
+document.write(date.getDay());	//5
+
+6. Выведите на экран текущий день недели (словом, по-русски). Создайте для этого вспомогательную функцию, 
+которая параметром принимает число, а возвращает день недели по-русски.
+function weekDay(num)
+{
+  var arrWeek = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
+  document.write(arrWeek[num]);
+}
+var date = new Date();
+weekDay(date.getDay());
+//пт
+-----------------------------------
+7. Узнайте, какой день недели был 7-го января 2015 года.
+function weekDay(num)
+{
+  var arrWeek = ['вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб'];
+  document.write(arrWeek[num]);
+}
+var date = new Date();
+weekDay(date.getDay()); 
+//сб
+-------------------------- .getTime() -------------------------
+8.	Выведите на экран количество минут с 1-го января 1970 года до настоящего момента времени.
+var date = new Date();
+var milSec = date.getTime();
+document.write('Din 1 ianuarie 1970 au trecut: '+ parseInt(milSec/60000) + ' minute.')
+//Din 1 ianuarie 1970 au trecut: 24808659 minute.
+-------------------------- Date.parse() -----------------------
+9.	Выведите на экран количество секунд, прошедшее между 1 марта 1988 года и текущим моментом с помощью Date.parse.
+var date = new Date();
+var now = date.getTime();
+var past = Date.parse('1988-02-01'); 
+document.write((now - past)/1000); 
+//917767943.668    
+---------------------
+10.	Выведите на экран количество секунд с начала дня до настоящего момента времени.
+var date = new Date();
+var startDay = new Date(date.getFullYear(), date.getMonth(), date.getDate());
+var now = date.getTime();                            
+var start = startDay.getTime();
+document.write((now-start)/1000 + '<br>');    
+//29078.376
+--------------------
+11.	Создайте инпут, в который пользователь вводит дату своего рождения в формате '2014-12-31' 
+(с конкретным годом). По потери фокуса выведите под инпутом сколько дней осталось до его дня рождения. 
+Воспользуйтесь методом Date.parse.
+/*
+<input type="text" onmouseout="func()" id="input" placeholder="yyyy-mm-dd">
+<br><span id="out"></span>  
+*/
+function func()
+{
+  var intro = document.getElementById('input').value;
+  var out = document.getElementById('out');
+  var introChange = '1970-' + Number(intro.substr(5, 2)-1) + intro.substr(7, 3);
+  var introParse = Date.parse(introChange);
+  var date = new Date();
+  var dateStr = '1970-' + date.getMonth() + '-' + date.getDate();
+  var dateParse = Date.parse(dateStr);
+  var deffMilsec = Math.abs(introParse - dateParse);
+  var deffDay = Math.round(deffMilsec/(1000*60*60*24));
+  out.innerHTML = 'V-au ramas: ' + deffDay + ' de zile';
+}
+//start Time: Date.parse('1970-01-01T02:00:00')
+
+Source:playcode.io/7460
+Public:7460.playcode.io
