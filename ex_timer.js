b49ccbd6ace0833fc93506cc1dd1ba5cd0aeda65
Spinu Vladimir <spinu.vlad@gmail.com>
2017-03-09 10:44:12 +0200

    dfd

--- ex_timer.js
+++ ex_timer.js
@@ -204,3 +204,93 @@ function disInput(elem)
 }
 -----------------------------------------------------
 7. Создайте карусель:
+//    playcode.io/7855
+/*
+<div>
+  <img src="http://theory.phphtml.net/exercises/javascript/base/zadachi-na-tajmery-javascript/smiles/1.png" alt="">
+  <img src="http://theory.phphtml.net/exercises/javascript/base/zadachi-na-tajmery-javascript/smiles/2.png" alt="">
+  <img src="http://theory.phphtml.net/exercises/javascript/base/zadachi-na-tajmery-javascript/smiles/3.png" alt="">
+  <img src="http://theory.phphtml.net/exercises/javascript/base/zadachi-na-tajmery-javascript/smiles/4.png" alt="">
+  <img src="http://theory.phphtml.net/exercises/javascript/base/zadachi-na-tajmery-javascript/smiles/5.png" alt="">
+  <img src="http://theory.phphtml.net/exercises/javascript/base/zadachi-na-tajmery-javascript/smiles/6.png" alt="">
+  <input type="submit" onclick="start()" id="input-start" value="Запустить карусель!">
+  <input type="submit" onclick="stop()" id="input-stop" disabled value="Остановить карусель!!">
+</div>
+*/
+
+function start()
+{
+  var inputStart = document.getElementById('input-start');
+  var inputStop = document.getElementById('input-stop');
+  var imgs = document.getElementsByTagName('img');
+  var temp = imgs[0].src;
+  imgs[0].src = imgs[1].src;
+  imgs[1].src = imgs[2].src;
+  imgs[2].src = imgs[3].src;
+  imgs[3].src = imgs[4].src;
+  imgs[4].src = imgs[5].src;
+  imgs[5].src = temp;
+  window.timerId = window.setTimeout(start, 1000);
+  inputStart.disabled = true;
+  inputStop.disabled = false;
+}
+
+function stop()
+{
+  var inputStop = document.getElementById('input-stop');
+  var inputStart = document.getElementById('input-start')
+  inputStart.disabled = false;
+  inputStop.disabled = true;
+  window.clearInterval(window.timerId);
+}
+--------------------------------------------------------
+8. Создайте отсчет до полуночи:
+//    playcode.io/7861
+/*
+<body onload="timer()">
+  <p>До полуночи осталось:</p>
+<table>
+  <tr>
+    <th>Часы</th>
+    <th>Минуты</th>
+    <th>Секунды</th>
+  </tr>
+  <tr>
+    <td id="hour"></td>
+    <td id="min"></td>
+    <td id="sec"></td>
+  </tr>
+</table>
+</body>
+*/
+
+function timer()
+{
+  var hour = document.getElementById('hour');
+  var min = document.getElementById('min');
+  var sec = document.getElementById('sec');
+  var nowDate = new Date();
+  var endDate = new Date(nowDate.getFullYear(), nowDate.getMonth(), nowDate.getDate() + 1)
+  var rest = endDate - nowDate;
+  var secondsFull = Math.floor(rest/1000); 
+  var hourRest = secondsFull % (60*60*24);
+  var minRest = hourRest % (60*60);
+  var secRest = minRest % 60;
+      
+  hour.innerHTML = addZero(Math.floor(hourRest/(60*60)));
+  min.innerHTML = addZero(Math.floor(minRest/60));
+  sec.innerHTML = addZero(Math.floor(secRest));
+  window.setTimeout(timer, 1000);
+}
+
+function addZero(num)
+{
+  if (num < 10)
+  {
+    return '0' + num; 
+  }
+  else
+  {
+    return num;
+  }
+}
