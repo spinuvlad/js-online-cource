627642f66049a141eeeea06d93e30d9c8304d901
Spinu Vladimir <spinu.vlad@gmail.com>
2017-03-20 14:26:59 +0200

    fdef

---
+++ dom.js
@@ -0,0 +1,447 @@
+------------------------- classList ------------------------
+Свойство classList содержит псевдомассив который содержит все классы элемента. Кроме того, 
+у classList есть несколько методов для работы с классами.
+псевдомассив: http://theory.phphtml.net/javascript/pages/pseudo-array.html
+
+элемент.classList //Псеводомассив классов элемента 
+элемент.classList.add('класс') //Добавляет указанный класс 
+элемент.classList.remove('класс') //Удаляет указанный класс 
+элемент.classList.contains('класс') //Проверяет наличие указанного класса 
+элемент.classList.toggle('класс') //Добавляет класс, если его нет, удаляет если есть 
+----------------
+добавим элементу класс 'newClass':
+/*
+<p id="element" class="class"></p>
+*/
+var element = document.getElementById('element');
+element.classList.add('newClass');
+-----------------
+удалим класс 'newClass':
+
+var element = document.getElementById('element');
+element.classList.remove('newClass');
+------------------
+Проверим есть ли у элемента класс 'test':
+/*
+<p id="element" class="test"></p>
+*/
+var element = document.getElementById('element');
+var checkClass = element.classList.contains('test');
+document.write(checkClass);
+//true
+------------------------------ cssText ------------------------
+Свойство cssText позволяет задать CSS стили массово одной строкой. 
+При этом все содержимое атрибута style перезаписывается.
+/*
+<p id="element">Тестовый элемент</p>
+*/
+var element = document.getElementById('element');
+element.style.cssText = 'color: red; font-size: 20px;';
+-----------------
+Чтобы предыдущие стили не перезаписывались, можно сделать так:
+/*
+<p id="element" style="background: green">Тестовый элемент</p>
+*/
+var element = document.getElementById('element');
+element.style.cssText += 'color: red; font-size: 20px;';
+//	<p id="element" style="background: green; color: red; font-size: 20px;">Тестовый элемент</p>
+---------------------------- tagName ---------------------------
+Свойство tagName содержит имя тега в верхнем регистре (большими буквами).
+// <div id="element"></div>
+var element = document.getElementById('element');
+document.write(element.tagName);
+// DIV
+---------------------------- createElement --------------------------- Вставка элементов
+Метод createElement позволяет создать новый элемент, передав в параметре имя тега.
+После создания с элементом можно работать как с обычным элементом, а также его можно добавить на страницу.
+/*
+<div id="parent"></div>
+*/
+var parent = document.getElementById('parent');
+
+var p = document.createElement('p');
+p.innerHTML = 'Содержимое абзаца';
+
+parent.appendChild(p);
+/*
+<div id="parent">
+	<p>Содержимое абзаца</p>
+</div>
+*/
+-------------------------
+Создадим картинку:
+/*
+<div id="parent"></div>
+*/
+var parent = document.getElementById('parent');
+
+var img = document.createElement('img');
+img.src = '/img/js.png';
+img.alt = 'JavaScript';
+img.title = 'JavaScript';
+
+parent.appendChild(img);
+/*
+<div id="parent">
+	<img src="/img/js.png" alt="JavaScript" title="JavaScript">
+</div>
+*/
+------------------------------- appendChild ---------------------------- Вставка элементов
+Метод appendChild позволяет вставить в конец элемента переданный элемент. 
+Чаще всего используется после создания элемента с помощью createElement.
+
+Создадим абзац, и поместим его на страницу в блок с id равным parent:
+/*
+<div id="parent"></div>
+*/
+var parent = document.getElementById('parent');
+
+var img = document.createElement('img');
+img.src = '/img/js.png';
+
+parent.appendChild(img);
+
+/*
+<div id="parent">
+	<img src="/img/js.png">
+</div>
+*/
+------------------------------ insertBefore ----------------------------- Вставка элементов
+Метод insertBefore позволяет вставить элемент в произвольное место элемента (перед переданным дочерним элементом). 
+Используется, например, после создания элемента с помощью createElement.
+
+Создадим абзац, и поместим его до второго абзаца:
+/*
+<div id="parent">
+	<p>Абзац</p>
+	<p>Абзац</p>
+	<p>Абзац</p>
+</div>
+*/
+var parent = document.getElementById('parent');
+
+var p = document.createElement('p');
+p.innerHTML = 'Содержимое нового абзаца';
+
+parent.insertBefore(p, parent.children[1]);	// parent.children[1] - это дочерним элемент, до которого нужно вставить элемент.
+/*
+<div id="parent">
+	<p>Абзац</p>
+	<p>Содержимое нового абзаца</p>
+	<p>Абзац</p>
+	<p>Абзац</p>
+</div>
+*/
+---------------------
+Добавим параграф в начало элемента с id равным parent:
+parent.insertBefore(p, parent.firstChild);
+
+добавлять в начало элемента можно даже тогда, когда в нем нет дочерних элементов:
+/*
+<div id="parent"></div>
+*/
+var parent = document.getElementById('parent');
+
+var p = document.createElement('p');
+p.innerHTML = 'Содержимое нового абзаца';
+
+parent.insertBefore(p, parent.firstChild);
+
+/*
+<div id="parent">
+	<p>Содержимое нового абзаца</p>
+</div>
+*/
+-------------------------------- insertAdjacentHTML ------------------------------ Вставка элементов
+Метод insertAdjacentHTML позволяет вставлять HTML код в любое место страницы.
+
+можно вставить html код без insertAdjacentHTML:
+/*
+<div id="element">
+	<p>Абзац</p>
+</div>
+*/
+var element = document.getElementById('element');
+element.innerHTML +='<p>Новый абзац</p>';
+/*
+<div id="element">
+	<p>Абзац</p>
+	<p>Новый абзац</p>
+</div>
+*/
+------------------------
+insertAdjacentHTML мы добавили HTML код не затронув остальные элементы. 
+Следовательно все назначенные события останутся. Помимо того такой способ более гибкий и эффективный.
+
+var element = document.getElementById('element');
+element.insertAdjacentHTML('beforeEnd', '<p>Новый абзац</p>');
+/*
+<div id="element">
+	<p>Абзац</p>
+	<p>Новый абзац</p>
+</div>
+*/
+'beforebegin'
+Before the element itself.
+'afterbegin'
+Just inside the element, before its first child.
+'beforeend'
+Just inside the element, after its last child.
+'afterend'
+After the element itself.
+---------------------------------- children ------------------------------- Потомки
+Свойство children хранит в себе псевдомассив дочерних элементов.
+
+С помощью innerHTML выведем содержимое первого дочернего элемента блока с идентификатором "parent":
+
+/*
+<div id="parent">
+	<p>Абзац 1</p>
+	<p>Абзац 2</p>
+</div>
+*/
+var parent = document.getElementById('parent');
+console.log(parent.children[0].innerHTML);
+// Абзац 1
+----------------------------------
+Также свойство childNodes содержит только непосредственных потомков элементов, 
+то есть только тех, которые лежат непосредственно внутри элемента.
+
+В данном примере у элемента с id 'parent' в свойстве children только один элемент, 
+не смотря на то, что в этом элементе также находится один элемент.
+/*
+<div id="parent">
+	<div>
+		<p>Параграф</p>
+	</div>
+</div>
+*/
+var parent = document.getElementById('parent');
+console.log(parent.children.length);
+// 1
+-----------------------------------
+всех потомков переберем в цикле, и выведем их содержимое:
+
+/*
+<div id="parent">
+	<p>Капуста</p>
+	<p>Морковка</p>
+	<p>Картошка</p>
+	<p>Фасоль</p>
+</div>
+*/
+var parent = document.getElementById('parent');
+var childs = parent.children;
+
+for (var i = 0; i < childs.length; i++){
+  var msg = (i + 1) + '. ' + childs[i].innerHTML;
+  document.write(msg + '<br>')
+}
+/*
+
+
+Капуста
+
+Морковка
+
+Картошка
+
+Фасоль
+1. Капуста
+2. Морковка
+3. Картошка
+4. Фасоль
+*/
+------------------------------- childNodes ------------------------------ Потомки
+Свойство childNodes хранит в себе псевдомассив непосредственных потомков элементов 
+(включая комментарии и текстовые узлы).
+/*
+<div id="parent">
+	<p>Абзац</p>
+	<!-- Коментарий -->
+</div>
+*/
+var parent = document.getElementById('parent');
+document.write(parent.childNodes.length);
+// 5
+-------------------
+Свойство childNodes содержит всех потомков, в том числе и комментарии и текстовые узлы (текст, и отступы между тегами). 
+Поэтому в предыдущем примере у элемента получилось 5 потомков.
+/*
+<div id="parent"><p>Абзац</p><!-- Коментарий --></div>
+*/
+var parent = document.getElementById('parent');
+console.log(parent.childNodes.length);
+ //2
+ ------------------------------- firstElementChild ----------------------------- Потомки
+ Свойство firstElementChild содержит первый дочерний элемент. 
+ Если у элемента нет дочерних элементов - возвращается null.
+ // firstChild - http://theory.phphtml.net/javascript/dom/firstChild.html
+/*
+ <div id="parent">
+	<p>Первый элемент</p>
+	<div>Последний элемент</div>
+</div>
+*/
+var parent = document.getElementById('parent');
+document.write(parent.firstElementChild.innerHTML);
+ //Первый элемент
+ ---------------
+ // <div id="parent"></div>
+ var parent = document.getElementById('parent');
+document.write(parent.firstElementChild);
+//null
+---------------------------- lastElementChild --------------------------- Потомки
+Свойство lastElementChild хранит в себе последний дочерний элемент. 
+Если у элемента нет дочерних элементов возвращается null.
+//lastChild - http://theory.phphtml.net/javascript/dom/lastChild.html
+/*
+<div id="parent">
+	<p>Первый элемент</p>
+	<div>Последний элемент</div>
+</div>
+*/
+var parent = document.getElementById('parent');
+document.write(parent.lastElementChild.innerHTML);
+//Последний элемент
+--------------------------- previousElementSibling ----------------------------- Соседи
+previousElementSibling содержит соседа сверху (предыдущий элемент).
+/*
+<p>Один</p>
+<p id="element">Два</p>
+*/
+var element = document.getElementById('element');
+var prev = element.previousElementSibling;
+document.write(prev.innerHTML);
+//Один
+--------------------------- nextElementSibling ---------------------------- Соседи
+/*
+<p id="element">Один</p>
+<p>Два</p>
+*/
+var element = document.getElementById('element');
+var prev = element.nextElementSibling;
+document.write(prev.innerHTML);
+//Два
+----------------------------- parentElement ------------------------------ Родители
+Свойство parentElement содержит родительский элемент.
+/*
+<div id="parent">
+	<p id="child"></p>
+</div>
+*/
+var child = document.getElementById('child');
+var parent = child.parentElement;
+document.write(parent.id);
+//parent
+-------
+var html = document.documentElement;
+document.write(html.parentElement);
+//null
+---------------------------- parentNode ----------------------------- Родители
+var html = document.documentElement;
+document.write(html.parentNode);
+//document
+----------------------------- cloneNode --------------------------- клонирование
+Метод cloneNode позволяет клонировать элемент и получить точную копию элемента. Которую затем можно вставить 
+на страницу с помощью appendChild или других подобных методов.
+
+В параметре получает true либо false. Если true, то клонирует элемент полностью, вместе со всем атрибутами и 
+дочерними элементами. Если false - только сам элемент (без дочерних элементов).
+/*
+<div id="parent">
+	<div class="block">
+		<p>Первый абзац</p>
+		<p>Второй абзац</p>
+	</div>
+</div>
+*/
+var parent = document.getElementById('parent');
+var block = parent.querySelector('.block');
+
+var clone = block.cloneNode(true);
+parent.appendChild(clone);
+/*
+<div id="parent">
+	<div class="block">
+		<p>Первый абзац</p>
+		<p>Второй абзац</p>
+	</div>
+
+	<div class="block">
+		<p>Первый абзац</p>
+		<p>Второй абзац</p>
+	</div>
+</div>
+*/
+------------------
+var parent = document.getElementById('parent');
+var block = parent.querySelector('.block');
+
+var clone = block.cloneNode(true);
+clone.children[0].innerHTML = 'Новое содержимое первого абзаца';
+clone.children[1].innerHTML = 'Новое содержимое второго абзаца';
+
+parent.appendChild(clone);
+/*
+<div id="parent">
+	<div class="block">
+		<p>Первый абзац</p>
+		<p>Второй абзац</p>
+	</div>
+
+	<div class="block">
+		<p>Новое содержимое первого абзаца</p>
+		<p>Новое содержимое второго абзаца</p>
+	</div>
+</div>
+*/
+----------------------------- removeChild ----------------------------- Удаление
+Метод removeChild позволяет удалить элемент. Применяется к родительскому элементу, 
+с указанием элемента, что нужно удалить.
+/*
+<div id="parent">
+	<p>Первый абзац</p>
+	<p id="element">Второй абзац</p>
+</div>
+*/
+var parent = document.getElementById('parent');
+var element = document.getElementById('element');
+
+parent.removeChild(element);
+/*
+<div id="parent">
+	<p>Первый абзац</p>
+</div>
+*/
+---------------------
+Удалим первый абзац:
+/*
+<div id="parent">
+	<p>Первый абзац</p>
+	<p>Второй абзац</p>
+</div>
+*/
+var parent = document.getElementById('parent');
+parent.removeChild(parent.firstElementChild);
+/*
+<div id="parent">
+	<p>Второй абзац</p>
+</div>
+*/
+---------------------
+Если нужно удалить элемент, но ссылки на его родителя нет, то можно поступить так:
+/*
+<div>
+	<p>Первый абзац</p>
+	<p id="element">Второй абзац</p>
+</div>
+*/
+var element = document.getElementById('element');
+
+element.parentNode.removeChild(element);
+/*
+<div id="parent">
+	<p>Первый абзац</p>
+</div>
+*/
---
+++ ex_dom.js
@@ -0,0 +1,330 @@
+1. Дан элемент #test. Добавьте ему класс .www
+//	<p id="test">Lorem ipsum dolor.</p>
+var text = document.getElementById('test');
+text.classList.add('www');
+---------------------------------------------
+2. Дан элемент #test. Удалите у него класс .www
+text.classList.remove('www');
+---------------------------------------------
+3. Дан элемент #test. Проверьте наличие у него класса .www
+alert(text.classList.contains('www'));
+//true
+----------------------------------------------
+4. Дан элемент #test. Добавьте ему класс .www, если его нет и удалите - если есть.
+text.classList.toggle('www');
+----------------------------------------------
+5. Дан элемент #test. Узнайте количество его классов.
+alert(text.classList.length);
+//1
+----------------------------------------------
+6. Дан элемент #test. Выведите последовательно алертом его классы.
+//	<p id="test" class="qqq eee rrr">Lorem ipsum dolor.</p>
+var text = document.getElementById('test');
+var arrClass = text.classList;
+for (var i = 0; i < arrClass.length; i++){
+  alert(arrClass[i]);        
+}  
+//qqq //eee //rrr
+---------------------------------------------- Работа с CSS
+7. Дан элемент #test. Сделайте его красного цвета, размером 30px, добавьте ему границу. 
+Решите задачу с помощью свойства cssText.
+var text = document.getElementById('test');
+text.style.cssText = 'color: red; font-size: 30px; border: 1px solid blue';
+
+---------------------------------------------- Свойство tagName
+8. Дан элемент #test. По клику на него выведите название его тега.
+var text = document.getElementById('test');
+text.addEventListener('click', func);
+
+function func(){
+  alert(this.tagName);
+}
+//P
+-----------------------------------------------
+9. Дан элемент #test. По клику на него выведите название его тега в нижнем регистре.
+var text = document.getElementById('test');
+text.addEventListener('click', func);
+
+function func(){
+  alert(this.tagName.toLowerCase());
+}
+-----------------------------------------------
+10. Даны элементы с классом .www. Добавьте каждому элементу в конец название его тега в нижнем регистре.
+/*
+<p class="www">Lorem ipsum dolor.</p>
+<p class="www">Sapiente, nisi, quas.</p>
+<p class="www">Quidem, quo, id!</p>
+<p>Lorem ipsum dolor.</p>
+<p class="www">Lorem ipsum dolor.</p>
+<p class="www">Repellendus, eligendi, architecto!</p>
+*/
+var text = document.querySelectorAll('.www');
+for (var i = 0; i < text.length; i++){
+  text[i].innerHTML += ' ' + text[i].tagName.toLowerCase();
+}
+------------------------------------------------ Вставка элементов
+11. Дан ol. Вставьте ему в конец li с текстом 'пункт'.
+/*
+<ol id="test">
+  <li></li>
+  <li></li>
+</ol>
+*/
+var test = document.getElementById('test');
+var list = document.createElement('li');
+list.innerHTML = 'punct';
+
+test.appendChild(list);
+/*
+1.
+2.
+3. punct
+*/
+------------------------------------------------
+12. Дан ul. Дан массив. Вставьте элементы этого массива в конец ul так, чтобы каждый элемент стоял в своем li.
+//	<ul id="test"></ul>
+var test = document.getElementById('test');
+var arrMonth = ['ian', 'feb', 'mart', 'apr', 'mai', 'iun', 'iul', 'aug', 'sept', 'oct', 'nov', 'dec'];
+
+for (var i = 0; i < arrMonth.length; i++){
+  var list = document.createElement('li');
+  list.innerHTML = arrMonth[i];
+  test.appendChild(list);
+}
+// *ian *feb ... *dec
+-------------------------------------------------
+13. Дан массив. Создайте div через createElement, затем вставьте каждый элемент этого массива 
+в отдельный p внутри этого div, затем вставьте этот div в конец body.
+
+var arrMonth = ['ian', 'feb', 'mart', 'apr', 'mai', 'iun', 'iul', 'aug', 'sept', 'oct', 'nov', 'dec'];
+var container = document.createElement('div');
+var body = document.getElementsByTagName('body');
+for (var i = 0; i < arrMonth.length; i++){
+  var p = document.createElement('p');
+  p.innerHTML = arrMonth[i];
+  container.appendChild(p);
+}
+body[0].innerHTML += container.innerHTML;
+-------------------------------------------------
+14. Дан элемент #test. Вставьте перед ним абзац с текстом '!!!'.
+// <h2 id="test">Lorem ipsum dolor sit.</h2>
+var test = document.getElementById('test');
+test.insertAdjacentHTML('beforebegin', '<p>!!!</p>');
+/*
+!!!
+Lorem ipsum dolor sit.
+*/
+--------------------------------------------------
+15. Дан элемент #test. Вставьте перед ним абзац с красным текстом '!!!' так, 
+чтобы к этому абзацу было привязано следующее событие: по клику на него он должен покраситься в зеленый цвет.
+// <h2 id="test">Lorem ipsum dolor sit.</h2>
+var test = document.getElementById('test');
+test.insertAdjacentHTML('beforebegin', '<p>!!!</p>');
+var pText = document.getElementsByTagName('p')[0];
+pText.style.color = 'red';
+pText.addEventListener('click', func);
+
+function func(){
+  this.style.color = 'green';
+}
+--------------------------------------------------
+16. Дан элемент #test. Найдите первого потомка этого элемента и сделайте его текст красного цвета.
+/*
+<ul id="test">
+  <li>Lorem ipsum dolor.</li>
+  <li>Blanditiis, placeat, cum.</li>
+  <li>Veritatis, esse, aspernatur.</li>
+  <li>Id, non, doloribus.</li>
+</ul>
+*/
+var test = document.getElementById('test');
+test.firstElementChild.style.color = 'red'; //test.children[0].style.color = 'red';
+---------------------------------------------------
+17. Дан элемент #test. Найдите последнего потомка этого элемента и сделайте его текст красного цвета.
+var test = document.getElementById('test');
+test.lastElementChild.style.color = 'red';
+---------------------------------------------------
+18. Дан элемент #test. Найдите всех потомков этого элемента и добавьте им в конец текст '!'.
+var test = document.getElementById('test');
+var allLi = test.children;
+for (var i = 0; i < allLi.length; i++){
+  allLi[i].innerHTML += ' !'
+}
+---------------------------------------------------
+19. Дан элемент #test. Найдите его соседа сверху и добавьте ему в конец текст '!'.
+/*
+<p>Lorem ipsum dolor.</p>
+<p id="test">Quo, temporibus doloremque.</p>
+<p>Veniam, alias, itaque.</p>
+*/
+var test = document.getElementById('test');
+var prev = test.previousElementSibling;
+prev.innerHTML += '!';
+----------------------------------------------------
+20. Дан элемент #test. Найдите его соседа снизу и добавьте ему в конец текст '!'.
+var test = document.getElementById('test');
+var next = test.nextElementSibling;
+next.innerHTML += '!';
+----------------------------------------------------
+21. Дан элемент #test. Найдите его соседа снизу его соседа снизу и добавьте ему в конец текст '!'.
+var test = document.getElementById('test');
+var next = test.nextElementSibling;
+var nextNext = next.nextElementSibling;
+nextNext.innerHTML += ' !';
+---
+var test = document.getElementById('test');
+var next = test.nextElementSibling.nextElementSibling;
+next.innerHTML += ' !';
+----------------------------------------------------
+22. Дан элемент #test. Найдите его родителя и покрасьте его в красный цвет.
+/*
+<div>
+  <p id="test">Lorem ipsum dolor sit amet.</p>
+</div>
+*/
+var test = document.getElementById('test');
+var parent = test.parentElement;
+parent.style.cssText = 'background: red';
+-----------------------------------------------------
+23. Дан элемент #test. Найдите родителя его родителя и покрасьте его в красный цвет.
+var test = document.getElementById('test');
+var parent = test.parentElement.parentElement;
+parent.style.cssText = 'background: red';
+-----------------------------------------------------
+24. Дан элемент #test. Найдите его родителя, найдите всех потомков этого родителя и поставьте им в конец текст '!'.
+/*
+<div>
+  <p>Lorem ipsum dolor sit amet.</p>
+  <p>Quis velit quibusdam totam. Vitae!</p>
+  <p>Optio, perspiciatis eos quas repudiandae!</p>
+  <p id="test">Labore, recusandae alias iusto id.</p>
+  <p>Laudantium, porro delectus nulla veniam.</p>
+</div>
+*/
+var test = document.getElementById('test');
+var parent = test.parentElement;
+var parentElem = parent.children;
+for (var i = 0; i < parentElem.length; i++){
+  parentElem[i].innerHTML += ' !';
+}
+------------------------------------------------------
+25. Дан элемент #test. Найдите его родителя, найдите соседа сверху и соседа снизу 
+для этого родителя и поставьте им в конец текст '!'.
+/*
+<h2>Lorem ipsum dolor</h2>
+<div>
+  <p>Lorem ipsum dolor sit amet.</p>
+  <p id="test">Labore, recusandae alias iusto id.</p>
+  <p>Laudantium, porro delectus nulla veniam.</p>
+</div>
+<h2>Lorem ipsum dolor</h2>
+*/
+var test = document.getElementById('test');
+var parent = test.parentElement;
+var prev = parent.previousElementSibling;
+var next = parent.nextElementSibling;
+prev.innerHTML += ' !';
+next.innerHTML += ' !';
+------------------------------------------------------
+26. Дан элемент #parent, внутри него дан элемент #test. Дана кнопка. 
+По нажатию на эту кнопку удалите элемент #test.
+/*
+<div id="parent">
+  <p id="test">Lorem ipsum dolor sit.</p>
+</div>
+<button onclick="func()">Delete</button>
+*/
+function func(){
+  var parent = document.getElementById('parent');
+  var test = document.getElementById('test');
+  parent.removeChild(test);
+}  
+------------------------------------------------------
+27. Дан ol. По нажатию на кнопку получите его последнего потомка и удалите его.
+/*
+<ol id="parent">
+  <li>Lorem ipsum dolor sit.</li>
+  <li>Asperiores animi pariatur dolorum.</li>
+  <li>Facere, eum adipisci explicabo.</li>
+  <li>Aspernatur accusamus odit neque.</li>
+  <li>Ex, sunt excepturi quos.</li>
+</ol>
+<button onclick="func()">Delete last</button>
+*/
+function func(){
+  var parent = document.getElementById('parent');
+  var lastElem = parent.lastElementChild;
+  parent.removeChild(lastElem);
+}  
+-----------------------------------------------------
+28. Дан элемент. Сделайте так, чтобы по нажатию по нему элемент удалялся.
+/*
+<p id="test">Lorem ipsum dolor sit.</p>
+*/
+var test = document.getElementById('test');
+var parent = test.parentElement;
+test.addEventListener('click', func);
+
+function func(){
+  parent.removeChild(test);
+}
+-----------------------------------------------------
+29. Дан инпут. Дана кнопка. По нажатию на кнопку клонируйте этот инпут.
+/*
+<form>
+  <input type="text" id="input">
+</form>
+<button onclick="func()">Clone</button>
+*/
+var input = document.getElementById('input');
+var parent = input.parentElement;
+
+var clone = parent.cloneNode(true);
+
+function func(){
+  parent.appendChild(clone);  
+} 
+//	playcode.io/8600
+-------------------------------------------------------
+30. Дан элемент #test. Найдите первого и последнего потомков этого элемента и поменяйте их тексты местами.
+/*
+<div id="test">
+  <p>Lorem ipsum dolor sit amet.</p>
+  <p>Non velit quae accusantium eligendi!</p>
+  <p>Eos, voluptate porro mollitia nostrum.</p>
+  <p>Ab hic earum reiciendis quae.</p>
+  <p>Aperiam, asperiores autem delectus ad!</p>
+</div>
+*/
+var test = document.getElementById('test');
+var first = test.firstElementChild;
+var last = test.lastElementChild;
+var temp = first.innerHTML;
+first.innerHTML = last.innerHTML;
+last.innerHTML = temp;
+------------------------------------------------------
+31. Дан элемент #test. Сделайте массив всех его родителей до тега html.
+/*
+<body>
+  <div>
+    <ul>
+      <li><a href="">link1</a></li>
+      <li><a href="" id="test">link2</a></li>
+      <li><a href="">link3</a></li>
+    </ul>
+  </div>
+</body>
+*/
+var test = document.getElementById('test');
+var arr = [];
+var i = 0;
+var parent = test.parentElement;
+while (parent !== null){
+  arr[i] = parent;
+  parent = parent.parentElement;
+  i++;
+}
+console.log(arr);
+-----------------------------------------------------
+32. Дан элемент #test. Среди всех его родителей найдите ближайшего родителя 
+с классом 'www' и поставьте ему в конец текст '!'.
